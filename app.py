cf332de5f0b60a36430d84e970c13ef98a5308e3
Sai Sahith <91070868+sai-sahith7@users.noreply.github.com>
2022-03-11 01:37:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,flash,send_from_directory,render_template,request,redirect,session,g,url_for
 import pyrebase
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "c6e803cd18a8c528c161eb9fcf013245248506ffb540ff70"
+
 firebaseConfig = {'apiKey': "AIzaSyAxKltWvPGTWzfL2qax0B8m6xnVcHdIqaM",
   'authDomain': "insight-3000.firebaseapp.com",
   'projectId': "insight-3000",
@@ -14,7 +16,6 @@ auth = firebase.auth()
 db = firebase.database()
 
 def get_individual_student_list(email):
-  email = email.replace("~","@").replace("`",".")
   c=1
   res = list()
   data_file = db.child("Data").get()
@@ -35,7 +36,6 @@ def get_complete_student_list():
   return res
 
 def get_individual_solver_list(email):
-  email = email.replace("~","@").replace("`",".")
   c = 1
   res = list()
   data_file = db.child("Data").get()
@@ -55,7 +55,6 @@ def check_solved(key):
         return True
 
 def get_solved_questions(email):
-  email = email.replace("~", "@").replace("`", ".")
   c = 1
   res = list()
   data_file = db.child("Data").get()
@@ -65,20 +64,19 @@ def get_solved_questions(email):
       c += 1
   return res
 
-@app.route("/login/",methods=["POST","GET"])
-def login_signup():
-    return render_template("login_page.html")
-
 @app.route("/signup/",methods=["POST","GET"])
 def signup():
   message=""
   ermessage = ""
+  session.pop('logged_in', None)
   if request.method == "POST":
     try:
       email = request.form["signup_email"]
       password = request.form["signup_password"]
       category = request.form["category"]
       auth.create_user_with_email_and_password(email, password)
+      session['logged_in'] = True
+      session['email'] = email
       message = "Successfully Registered"
       username = email.replace("@","~").replace(".","`")
       db.child("Category").update({ username : category })
@@ -91,30 +89,27 @@ def signin():
   message=""
   sumessage=""
   if request.method == "POST":
+    email = request.form["signin_email"]
+    password = request.form["signin_password"]
+    session['email'] = email
+    session['logged_in'] = True
     try:
-      email = request.form["signin_email"]
-      password = request.form["signin_password"]
       auth.sign_in_with_email_and_password(email, password)
       sumessage = "Successfully logged in"
-      temp = "/"+email.replace("@","~").replace(".","`")+"p="+password+"/"
-      print(temp)
-      return redirect(temp)
+      return redirect("/account/")
     except:
       message = "Invalid email or password.Try again"
   return render_template("login_page.html",ermessage=message,sumessage = sumessage)
 
-@app.route("/<string:inp>/",methods=["POST","GET"])
-def aftersignin(inp):
-  initial_list = inp.split("p=")
-  email = initial_list[0].replace("~","@").replace("`",".")
-  password = initial_list[1]
+@app.route("/account/",methods=["POST","GET"])
+def aftersignin():
   try:
-    auth.sign_in_with_email_and_password(email,password)
+    email = session["email"].replace("@","~").replace(".","`")
   except:
-    return redirect("/login/")
+    return redirect("/signin/")
   data_file = db.child("Category").get()
   for details in data_file.each():
-    if details.key() == initial_list[0]:
+    if details.key() == email:
       category = details.val()
   if request.method == "POST":
     if category == "Student":
@@ -123,7 +118,7 @@ def aftersignin(inp):
       info["Subject"] = request.form["Subject"]
       info["Contact details"] = request.form["Contact"]
       info["Question"] = request.form["Question"]
-      info["Student mail"] = email
+      info["Student mail"] = session["email"]
       info["Solver mail"] = "-"
       info["Answer"] = "-"
       db.child("Data").push(info)
@@ -131,20 +126,26 @@ def aftersignin(inp):
       key = request.form["key"]
       ans = request.form["answer"]
       if check_solved(key) is False:
-        db.child("Data").child(key).update({"Solver mail": email,"Answer":ans})
-    return redirect("/" + inp + "/")
+        db.child("Data").child(key).update({"Solver mail": session["email"],"Answer":ans})
+    return redirect("/account/")
   if category == "Student":
-    individual_student_list = get_individual_student_list(initial_list[0])    # returns a list of dictionary containing the details( argument given is changed email not the real one )
-    solved_data = get_solved_questions(initial_list[0])
-    return render_template("Student.html",inp="/"+inp+"/",data = individual_student_list , solved_data = solved_data)
+    individual_student_list = get_individual_student_list(session["email"])    # returns a list of dictionary containing the details( argument given is changed email not the real one )
+    solved_data = get_solved_questions(session["email"])
+    return render_template("Student.html",inp="/account/",data = individual_student_list , solved_data = solved_data)
   else:
     Student_list = get_complete_student_list()
-    Solver_list = get_individual_solver_list(initial_list[0]) # returns a list of dictionary containing the details( argument given is changed email not the real one )
-    return render_template("Solver.html",inp="/"+inp+"/",Student_list = Student_list,Solver_list = Solver_list)
+    Solver_list = get_individual_solver_list(session["email"]) # returns a list of dictionary containing the details( argument given is changed email not the real one )
+    return render_template("Solver.html",inp="/account/",Student_list = Student_list,Solver_list = Solver_list)
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    session.pop('logged_in', None)
+    session.pop('email', None)
+    return redirect("/signin/")
 
 @app.route("/")
 def redir():
-    return redirect("/login/")
+    return redirect("/signin/")
 
 if __name__ == "__main__":
     app.run()
